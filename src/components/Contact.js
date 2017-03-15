5200df300ffc73c0699197aeab19db309f23f91f
Sooji Noh <soojinoh@Soojiui-MacBook-Pro.local>
2017-03-16 08:48:33 +0900

    immunity helper in create,remove,edit handler

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
 import ContactDetails from './ContactDetails';
+import update from 'react-addons-update';
 
 export default class Contact extends React.Component {
   constructor(props) {
@@ -24,6 +25,10 @@ export default class Contact extends React.Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+
+    this.handleCreate = this.handleCreate.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
   }
 
   handleChange(e) {
@@ -36,7 +41,34 @@ export default class Contact extends React.Component {
     this.setState({
       selectedKey: key,
     });
-    console.log(key, 'is selected');
+  }
+
+  handleCreate(contact) {
+    this.setState({
+      contactData: update(this.state.contactData, { $push: [contact] }),
+    });
+  }
+
+  handleRemove() {
+    this.setState({
+      contactData: update(this.state.contactData,
+        { $splice: [[this.state.selectedKey, 1]] },
+      ),
+      selectedKey: -1,
+    });
+  }
+
+  handleEdit(name, phone) {
+    this.setState({
+      contactData: update(this.state.contactData,
+        {
+          [this.state.selectedKey]: {
+            name: { $set: name },
+            phone: { $set: phone },
+          },
+        },
+      ),
+    });
   }
 
   render() {
