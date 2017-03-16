97908e01b2c97a34b4188c6873d111006389b5ae
Sooji Noh <soojinoh@Soojiui-MacBook-Pro.local>
2017-03-16 12:05:19 +0900

    implement ContactEdit

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -1,6 +1,37 @@
 import React from 'react';
 
 export default class ContactDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEdit: false,
+      name: '',
+      phone: '',
+    };
+
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleToggle() {
+    if (!this.state.isEdit) {
+      this.setState({
+        name: this.props.contact.name,
+        phone: this.props.contact.phone,
+      });
+    } else {
+      this.props.onEdit(this.state.name, this.state.phone);
+    }
+    this.setState({
+      isEdit: !this.state.isEdit,
+    });
+  }
+
+  handleChange(e) {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  }
 
   render() {
     const details = (
@@ -10,11 +41,36 @@ export default class ContactDetails extends React.Component {
       </div>
     );
     const blank = (<div>Not Selected</div>);
+    const edit = (
+      <div>
+        <p>
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </p>
+        <p>
+          <input
+            type="text"
+            name="phone"
+            placeholder="phone"
+            value={this.state.phone}
+            onChange={this.handleChange}
+          />
+        </p>
+      </div>
+    );
+
+    const view = this.state.isEdit ? edit : details;
 
     return (
       <div>
         <h2>Details</h2>
-        {this.props.isSelected ? details : blank}
+        {this.props.isSelected ? view : blank}
+        <button onClick={this.handleToggle}>{this.state.isEdit ? 'Confirm' : 'Edit'}</button>
         <button onClick={this.props.onRemove}>Remove</button>
       </div>
     );
@@ -27,9 +83,11 @@ ContactDetails.defaultProps = {
     phone: '',
   },
   onRemove: () => {console.log('onRemove not defined'); },
+  onEdit: () => {console.log('onEdit not defined'); },
 };
 
 ContactDetails.propTypes = {
   contact: React.PropTypes.object,
   onRemove: React.PropTypes.func,
-}
+  onEdit: React.PropTypes.func,
+};
